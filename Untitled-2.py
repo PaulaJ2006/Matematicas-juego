5fc1a90aa8790b0c134a044a2fccf2d208d7d328
PaulaJ2006 <paula.bohorquez@correounivalle.edu.co>
2024-05-28 09:35:48 -0500

    Documento

---
+++ Untitled-2.py
@@ -0,0 +1,2 @@
+from matplotlib import *
+import matplotlib.pyplot as plt
---
+++ interfaz mate.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import customtkinter 
+
+root = customtkinter.CTk()
+root.geometry("1000x600")
+root.resizable(0,0)
+root.title("Descartico")
+root._set_appearance_mode("light")
+
